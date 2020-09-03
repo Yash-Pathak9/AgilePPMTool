123db26a4a5178af725e3617e2873cb6bcfc03f5
yash pathak <ypyashpathak@gmail.com>
2020-09-03 12:22:58 +0530

    Project Object

--- App/src/main/java/com/AgileIntelligence/App/Web/ProjectController.java
+++ App/src/main/java/com/AgileIntelligence/App/Web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.AgileIntelligence.App.Web;
 
 import com.AgileIntelligence.App.Domain.Project;
+import com.AgileIntelligence.App.Service.MapValidationErrorService;
 import com.AgileIntelligence.App.Service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,16 +22,15 @@ public class ProjectController {
 
     @Autowired
     private ProjectService projectService;
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
 
     @PostMapping
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
-        if(result.hasErrors()){
-            Map<String,String> errorMap = new HashMap<>();
-            for(FieldError error :result.getFieldErrors() ){
-                errorMap.put(error.getField(),error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String,String>> (errorMap,HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationErrorService(result);
+        if(errorMap!=null){
+            return errorMap;
         }
         Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
