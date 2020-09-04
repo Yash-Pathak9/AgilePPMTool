8d444c4ddcab099d7d77f8edeab32978e5a38e6d
yash pathak <ypyashpathak@gmail.com>
2020-09-04 06:37:28 +0530

    Project Object

--- App/src/main/java/com/AgileIntelligence/App/Web/ProjectController.java
+++ App/src/main/java/com/AgileIntelligence/App/Web/ProjectController.java
@@ -7,10 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
@@ -34,5 +32,10 @@ public class ProjectController {
         }
         Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectById(@PathVariable String projectId){
+        Project project = projectService.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project,HttpStatus.OK);
     }
 }
